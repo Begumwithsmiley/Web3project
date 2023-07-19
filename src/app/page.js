82b45a925f1fa311d2f70692ccf964600b6c1c7a
Begum SA <begumsener94@gmail.com>
2023-07-19 17:53:24 +0000

    landingpage

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Card from "./components/card";
 import Hero from "./components/hero";
 import Navbar from "./components/navbar";
 
@@ -5,9 +6,14 @@ import Navbar from "./components/navbar";
 export default function Home() {
   return (
     <div>
-    <Navbar/>
-    <Hero/>
-    
-  </div>
+      <Navbar />
+      <Hero />
+      <h1 className="text-4xl text-center p-10 font-extrabold">Shop Tax Free in Three Easy Steps</h1>
+      <div className="flex justify-center justify-evenly">
+      <Card/>
+      <Card/>
+      <Card/>
+      </div>
+     </div>
   )
 }
