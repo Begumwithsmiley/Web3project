a29bb1f83051dcaa390a946931b895e1baf99f07
Begum SA <begumsener94@gmail.com>
2023-07-19 19:47:19 +0000

    update footer

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <footer className="footer items-center p-4 bg-neutral text-neutral-content">
             <div className="items-center grid-flow-col">
-                <p className="text-left">Copyright © 2023 - Shop3</p>
+                <p className="text-left">Copyright © 2023 - Cr8Product</p>
                 </div>
                 <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
                 <p>Privacy Policy</p>
